e8d9c6c9310a622f0d67b31a82877e1f66764e00
ruijadom <ruijadomingues@gmail.com>
2020-05-02 15:26:38 +0100

    install plugins nprogress ganalytics gfonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,47 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Ruijdom`,
+    siteUrl: `https://ruijadomingues.netlify.app/`,
+    description: `Portfolio`,
+    social: [
+      {
+        name: "github",
+        url: "https://github.com/ruijadom",
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "231130702",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Cairo`,
+            subsets: [`latin`],
+          },
+          {
+            family: `Open Sans`,
+            variants: [`300`, `500`, `700`],
+          },
+        ],
+      },
+    }
+  ]
 }
